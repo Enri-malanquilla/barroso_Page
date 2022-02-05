bef192170984465cc503c26d106021b783c2ecb0
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2022-02-05 12:06:58 +0100

    creada base de datos

--- backend/bdd/initDB.js
+++ backend/bdd/initDB.js
@@ -7,8 +7,9 @@ async function initDB() {
     await connection.query('DROP TABLE IF EXISTS user_dev');
     await connection.query('DROP TABLE IF EXISTS photos_song_event');
     await connection.query('DROP TABLE IF EXISTS song_author');
-    await connection.query('DROP TABLE IF EXISTS video_rrss');
-    await connection.query('DROP TABLE IF EXISTS audio_flamenco');
+    // await connection.query('DROP TABLE IF EXISTS video_rrss');
+    // await connection.query('DROP TABLE IF EXISTS audio_flamenco');
+    await connection.query('DROP TABLE IF EXISTS event_news');
     await connection.query('DROP TABLE IF EXISTS partners_colaboration');
     console.log('TABLES REMOVED');
 
@@ -33,6 +34,80 @@ async function initDB() {
     `);
 
     console.log('USER TABLE CREATED');
+
+    await connection.query(`
+    CREATE TABLE partners_colaboration (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(50),
+        comercial_name VARCHAR(50) NOT NULL,
+        type_partner ENUM("singer", "productor"),
+        created_partner DATETIME NOT NULL,
+        modified_partner DATETIME
+    )
+    `);
+
+    console.log('PARTNER TABLE CREATED');
+
+    await connection.query(`
+        CREATE TABLE song_author (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            spotyfy VARCHAR(100),
+            youtube VARCHAR(100),
+            applemusic VARCHAR(100),
+            itunes VARCHAR(100),
+            titulo VARCHAR(50) NOT NULL,
+            song_type ENUM("propia", "cover", "colaboracion", "propia-colaboracion") NOT NULL,
+            created_song DATETIME NOT NULL,
+            id_singer INT NOT NULL,
+            FOREIGN KEY (id_singer) REFERENCES 
+            partners_colaboration(id) ON DELETE CASCADE,
+            id_collaborator_1 INT,
+            FOREIGN KEY (id_collaborator_1) REFERENCES 
+            partners_colaboration(id) ON DELETE CASCADE,
+            id_collaborator_2 INT,
+            FOREIGN KEY (id_collaborator_2) REFERENCES 
+            partners_colaboration(id) ON DELETE CASCADE,
+            id_productor INT NOT NULL,
+            FOREIGN KEY (id_productor) REFERENCES 
+            partners_colaboration(id) ON DELETE CASCADE,
+            actived_song BOOLEAN DEFAULT true
+            
+        )
+    `);
+
+    console.log('SONGS TABLE CREATED');
+
+    await connection.query(`
+    CREATE TABLE event_news (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(50) NOT NULL,
+        description TEXT,
+        type_news ENUM("articulo", "radio", "concierto", "lanzamiento"),
+        enlace VARCHAR(100),
+        active_event BOOLEAN DEFAULT true,
+        created DATETIME NOT NULL,
+        finish_date DATETIME
+    )
+    `);
+    console.log('EVENT TABLE CREATED');
+
+    await connection.query(`
+        CREATE TABLE photos_song_event (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            type_photo ENUM('song', 'bio', 'random') NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            description VARCHAR(50),
+            created_photo DATETIME NOT NULL,
+            id_song INT,
+            FOREIGN KEY (id_song) REFERENCES
+            song_author(id) ON DELETE CASCADE,
+            id_event INT,
+            FOREIGN KEY (id_event) REFERENCES
+            event_news(id) ON DELETE CASCADE
+        )
+    `);
+
+    console.log('PHOTOS TABLES CREATED');
   } catch (error) {
     console.error(error.message);
   } finally {
